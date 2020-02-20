55a57fd0d470db4287dc8d05b43aa5c35b542cab
ScottYijun <chenyijunsz@163.com>
2020-02-21 00:22:00 +0800

    第1章第一题

--- chapter01/example01.c
+++ chapter01/example01.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
 
+#define MAXSIZE 10
+
+typedef int ElementType;
+
+void Create(ElementType x[], int *n)
+{
+    int i;
+    printf("\n请输入数据个数据n=");
+    scanf_s("%d", &(*n));
+    printf("请输入%d数据：",*n);
+    for(i = 0; i < *n; ++i)
+    {
+        scanf_s("%d", &x[i]);
+    }
+}
+
+void Maxmin(ElementType x[], int n, int *max, int *min)
+{
+    int i;
+    *max = *min = x[1];
+    for(i = 0; i < n; ++i)
+    {
+        if(x[i] > *max)
+            *max = x[i];
+        if(x[i] < *min)
+            *min = x[i];
+    }
+}
+
 int main()
 {
-    printf("Hello World!\n");
+    ElementType data[MAXSIZE + 1];
+    int i, n, max, min;
+    Create(data, &n);
+    printf("\n数组创建完毕！ \n所输入的数据是：");
+    for(i = 0; i < n; ++i)
+        printf("%4d", data[i]);
+    Maxmin(data, n, &max, &min);
+
+    printf("\n最大值是：%d\t最小值是：%d\n", max, min);
     return 0;
 }
+
+
